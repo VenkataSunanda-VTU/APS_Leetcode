2711464e5caadd9a921aa24469567af8443a516f
Venkata_Sunanda_VTU <vtu26377@veltech.edu.in>
2026-04-16 09:10:09 +0530

    Time: 15 ms (82.02%), Space: 45.9 MB (96.41%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int INF = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == INF ? -1 : dp[amount];
+    }
+}
